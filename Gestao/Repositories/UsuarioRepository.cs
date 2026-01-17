4a27aaed636c570ddd04c8a8220eabea15101620
MrBeYd <alexandrerui993@gmail.com>
2026-01-17 16:51:18 +0100

    Adicionar arquivos de projeto.

---
+++ Gestao/Repositories/UsuarioRepository.cs
@@ -0,0 +1,55 @@
+﻿using Catel;
+using Gestao.Data;
+using Gestao.Models;
+using Gestao.Services;
+using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gestao.Repositories
+{
+    public class UsuarioRepository
+    {
+        
+        [Obsolete]
+        public Usuario Login(string username, string senha)
+        {
+            using (var conn = DbConnection.GetConnection())
+            {
+                string hash = HashService.GerarHash(senha);
+
+                string sql = @"SELECT Id, Nome, Perfil 
+                       FROM usuarios
+                       WHERE Username = @username
+                       AND SenhaHash = @senha
+                       AND Ativo = 1";
+
+                using (MySqlCommand cmd = new MySqlCommand(sql, conn))
+                {
+                    cmd.Parameters.AddWithValue("@username", username);
+                    cmd.Parameters.AddWithValue("@senha", hash);
+
+                    conn.Open();
+
+                    using (MySqlDataReader dr = cmd.ExecuteReader())
+                    {
+                        if (dr.Read())
+                        {
+                            return new Usuario
+                            {
+                                Id = Convert.ToInt32(dr["Id"]),
+                                Nome = dr["Nome"].ToString(),
+                                Perfil = dr["Perfil"].ToString()
+                            };
+                        }
+                    }
+                }
+            }
+
+            return null;
+        }
+    }
+}
---
+++ Gestao/Views/LoginFrm.cs
@@ -0,0 +1,62 @@
+﻿using Gestao.Repositories;
+using Gestao.Services;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Gestao.Views
+{
+    public partial class LoginFrm : Form
+    {
+        public LoginFrm()
+        {
+            InitializeComponent();
+        }
+
+        private void panel2_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void LoginFrm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void loginBtn_Click(object sender, EventArgs e)
+        {
+            UsuarioRepository repo = new UsuarioRepository();
+            var usuario = repo.Login(txtUsername.Text, txtSenha.Text);
+
+            if (usuario == null)
+            {
+                MessageBox.Show("Usuário ou senha inválidos.",
+                    "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            SessionManager.UsuarioId = usuario.Id;
+            SessionManager.Nome = usuario.Nome;
+            SessionManager.Perfil = usuario.Perfil;
+            Form1 dashboard = new Form1(usuario.Perfil);
+            dashboard.Show();
+            this.Hide();
+        }
+    }
+}
